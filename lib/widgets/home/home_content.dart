96d46b7afc0829e6eabae05ccddd90e4a4074f60
Davi Holanda <davirolim94@gmail.com>
2022-05-16 07:48:27 -0300

    refactoring the home content separating widgets

--- lib/widgets/home/home_content.dart
+++ lib/widgets/home/home_content.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:read_smart/helpers/custom_page_route.dart';
 import 'package:read_smart/screens/daily_review_screen.dart';
+import 'package:read_smart/widgets/home/headline_card.dart';
+import 'package:read_smart/widgets/home/section_card.dart';
 
 import '../../providers/daily_review_provider.dart';
 
@@ -16,124 +18,73 @@ class HomeContent extends ConsumerStatefulWidget {
 class _HomeContentState extends ConsumerState<HomeContent> {
   @override
   void initState() {
-    final dailyReviewProvider = ref.read(DailyReviewProvider.dailyReviewProvider);
+    // TODO Should Emit the HomeContentInitEvent
+    // and the provider would run the code below
+    final dailyReviewProvider =
+        ref.read(DailyReviewProvider.dailyReviewProvider);
     if (dailyReviewProvider.dailyReview.highlights.isEmpty) {
+      print('entrou');
       ref.read(DailyReviewProvider.dailyReviewProvider).getDailyReview();
     }
     super.initState();
   }
 
+  final _image = AssetImage("assets/images/daily-review-bg.webp");
+
+  final _textTop = DateFormat.yMMMMd().format(DateTime.now());
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.black54,
-        margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        child: Column(
-          children: [
-            _buildPrimaryCard(context),
-            SizedBox(height: 18.0),
-            _buildSecondaryCard(context, 'Books', null),
-            SizedBox(height: 5.0),
-            _buildSecondaryCard(context, 'Summaries', null),
-          ],
-        ));
-  }
-
-  Container _buildSecondaryCard(
-      BuildContext context, String title, Function? ontap) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.12,
-      width: double.infinity,
-      child: InkWell(
-        onTap: () => ontap,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            side: BorderSide(width: 1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          color: Color(0xff9d6790),
-          elevation: 2,
-          child: Container(
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.headlineMedium,
-              )),
-        ),
-      ),
-    );
-  }
-
-  Container _buildPrimaryCard(BuildContext context) {
     final todayProgressText =
         ref.watch(DailyReviewProvider.dailyReviewProvider).progressText;
     final finishedReview =
         ref.read(DailyReviewProvider.dailyReviewProvider).dailyReview.finished;
     return Container(
-      height: MediaQuery.of(context).size.height * 0.30,
-      width: double.infinity,
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(CustomPageRoute(DailyReviewScreen()));
-        },
-        child: Card(
-          elevation: 5,
-          color: Colors.transparent,
-          child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: AssetImage("assets/images/daily-review-bg.webp"),
-                    fit: BoxFit.cover,
-                    opacity: 0.65),
-              ),
-              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    DateFormat.yMMMMd().format(DateTime.now()),
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  SizedBox(height: 8),
-                  Divider(
-                    height: 10,
-                    thickness: 2,
-                    color: Colors.black,
-                  ),
-                  SizedBox(height: 13),
-                  Text('Daily Review',
-                      style: Theme.of(context).textTheme.titleLarge),
-                  Flexible(
-                    child: Container(
-                      alignment: Alignment.bottomCenter,
-                      child: Row(
-                        children: [
-                          if (finishedReview)
-                            ClipOval(
-                              child: Material(
-                                color: Colors.grey[100],
-                                child: SizedBox(
-                                  child: Icon(
-                                    Icons.done_rounded,
-                                    color: Colors.black,
-                                    size: 15,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          SizedBox(width: 5),
-                          Text(
-                            todayProgressText ?? '',
-                            style: Theme.of(context).textTheme.headlineSmall,
-                          ),
-                        ],
-                      ),
+        color: Colors.black54,
+        margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        child: Column(
+          children: [
+            HeadlineCard(
+                title: 'Daily Review',
+                image: _image,
+                textTop: _textTop,
+                body: _buildBody(context, finishedReview, todayProgressText),
+                ontap: () => Navigator.of(context)
+                    .push(CustomPageRoute(DailyReviewScreen()))),
+            SizedBox(height: 18.0),
+            SectionCard(title: 'Books'),
+            SizedBox(height: 5.0),
+            SectionCard(title: 'Summaries'),
+          ],
+        ));
+  }
+
+  _buildBody(
+      BuildContext context, bool finishedReview, String? todayProgressText) {
+    return Flexible(
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          children: [
+            if (finishedReview)
+              ClipOval(
+                child: Material(
+                  color: Colors.grey[100],
+                  child: SizedBox(
+                    child: Icon(
+                      Icons.done_rounded,
+                      color: Colors.black,
+                      size: 15,
                     ),
-                  )
-                ],
-              )),
+                  ),
+                ),
+              ),
+            SizedBox(width: 5),
+            Text(
+              todayProgressText ?? '',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
         ),
       ),
     );
