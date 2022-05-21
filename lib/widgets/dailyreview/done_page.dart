00a74c9dd2e21685037b82a0864436e48d3883ee
Davi Holanda <davirolim94@gmail.com>
2022-05-21 10:14:44 -0300

    adding new Done padge widget

---
+++ lib/widgets/dailyreview/done_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DonePage extends ConsumerWidget {
+  const DonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
